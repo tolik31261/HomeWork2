65128e6e3a8a357fde95bb701518ca13633052aa
User <nazarovtolya2@gmail.com>
2023-02-02 21:36:09 +0500

    Выполнение задач 1-3

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,39 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-         // Пишем код для задачи 1
+     var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);// Пишем код для задачи 1
     }
-
     public static void task2 () {
         System.out.println("Задача 2");
-         // Пишем код для задачи 2
-    }
+        var dog = 8.0;
+        dog = dog + 4;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat);
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper);
 
+        // Пишем код для задачи 2
+    }
 
     public static void task3() {
         System.out.println("Задача 3");
+        var dog = 12.0;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 7.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
+        var paper = 763793;
+        paper = paper - 7639;
+        System.out.println(paper);
+
     }
 }
